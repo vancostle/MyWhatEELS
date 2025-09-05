7cfa3b78c50121e326066b35edff23f4bcb435f0
vcostale <vcostale7@gmail.com>
2025-09-05 16:52:09 +0200

    usage of spectrum image visualizer for spectrum line visualizer

---
+++ whateels/pages/home/MVC/view/eels_plots/spectrum_line_visualizer/__init__.py
@@ -0,0 +1,6 @@
+"""
+Paquete spectrum_line_visualizer: expone la clase SpectrumLineVisualizer (alias de la vista).
+"""
+from .view import SpectrumLineView as SpectrumLineVisualizer
+
+__all__ = ["SpectrumLineVisualizer"]
