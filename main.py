b0761009869a19a93613f4222ead8e625c315fa4
Andry <andry.html@gmail.com>
2025-09-23 10:10:07 +0200

    Nothing important

--- main.py
+++ main.py
@@ -8,21 +8,17 @@ APP_NAME = "WhatEELS"
 PORT = 5006
 
 if __name__ == "__main__":
-    try:
-        # Start Panel server in a background thread
-        t = threading.Thread(
-            target=lambda: App(title=APP_NAME).run(port=PORT, show=False),
-            daemon=True
-        )
-        t.start()
-        # Wait for server to start (adjust if needed)
-        time.sleep(2)
-        # Open PyWebView window to localhost
-        webview.create_window(
-            APP_NAME, 
-            f'http://localhost:{PORT}',
-        )
-        webview.start()
-    except KeyboardInterrupt:
-        print("Shutting down...")
-        webview.destroy_window()
+    # Start Panel server in a background thread
+    t = threading.Thread(
+        target=lambda: App(title=APP_NAME).run(port=PORT, show=False),
+        daemon=True
+    )
+    t.start()
+    # Wait for server to start (adjust if needed)
+    time.sleep(2)
+    # Open PyWebView window to localhost
+    webview.create_window(
+        APP_NAME, 
+        f'http://localhost:{PORT}',
+    )
+    webview.start()
