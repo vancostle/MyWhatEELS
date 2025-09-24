8062580f29233944ab6af1907af3482520838c31
vcostale <vcostale7@gmail.com>
2025-09-24 11:34:23 +0200

    eels_plots folder deleted

--- whateels/pages/home/MVC/view/eels_plots/spectrum_image_visualizer/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Paquete espectrum_image_visualizer: expone la clase SpectrumImageVisualizer (alias de la vista).
-"""
-from .view import SpectrumImageView as SpectrumImageVisualizer
-
-__all__ = ["SpectrumImageVisualizer"]
--- whateels/pages/home/MVC/view/eels_plots/spectrum_line_visualizer/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Paquete spectrum_line_visualizer: expone la clase SpectrumLineVisualizer (alias de la vista).
-"""
-from .view import SpectrumLineView as SpectrumLineVisualizer
-
-__all__ = ["SpectrumLineVisualizer"]
