d07b4ea16a025ca00c10cd0eb194b172e2ee88a3
Andry <andry.html@gmail.com>
2025-10-27 12:43:05 +0100

    Testing colors

--- whateels/components/custom_page.py
+++ whateels/components/custom_page.py
@@ -19,7 +19,7 @@ class CustomPage(pn.template.FastListTemplate):
     """
     
     _DEFAULT_TITLE = "Custom Page"
-    _DEFAULT_HEADER_BACKGROUND = "#4caf50"
+    _DEFAULT_HEADER_BACKGROUND = "#4d4dc9"
 
     def __init__(
         self, 
