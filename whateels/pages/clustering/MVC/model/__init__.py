ddeafdeddf1c88457aab2b8a954f4366ca88a25c
Andry <andry.html@gmail.com>
2025-11-07 12:23:34 +0100

    Add Placeholders class and integrate into ClusteringModel
    Introduced a new Placeholders class for loading HTML templates and added it to the ClusteringModel. Updated __init__.py to provide access to placeholders via a property, improving template management and separation of concerns.

--- whateels/pages/clustering/MVC/model/__init__.py
+++ whateels/pages/clustering/MVC/model/__init__.py
@@ -1,19 +1,24 @@
-from whateels.base.mvc import BaseModel
-from whateels.shared_state import AppState
-from .constants import Constants
 import numpy as np
+
+from whateels.shared_state import AppState
+from .placeholders import Placeholders
+from .constants import Constants
 from typing import Optional
 
-class ClusteringModel(BaseModel):
+class ClusteringModel:
     def __init__(self):
         super().__init__()
-
+        
+        self._placeholders = Placeholders()
         self._constants = Constants()
         self._app_state = AppState()
         
     @property
     def constants(self):
         return self._constants
+    @property
+    def placeholders(self) -> Placeholders:
+        return self._placeholders
     @property
     def app_state(self) -> AppState:
         """
