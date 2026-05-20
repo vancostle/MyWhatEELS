fbc55e2c2a32ad94d77ad7fbc1cf3ceaaaee8c89
vcostale <vcostale7@gmail.com>
2026-05-20 23:49:03 +0200

    Rename Clustering 2 to Adv. Clustering
    Update UI labels and page titles to use "Adv. Clustering" instead of "Clustering 2" (pages/clustering_2 and templates). Also change placeholder animation names in TODO_clustering_page_empty.html to use pulse-adv-clustering. Note: the CSS animation lines and one page title contain minor formatting inconsistencies (missing duration tokens and a missing space) that may need a follow-up fix.

--- whateels/templates/general_page_template.py
+++ whateels/templates/general_page_template.py
@@ -159,9 +159,9 @@ class GeneralPageTemplate(pn.template.FastListTemplate):
         
         clustering_2_href = f'/clustering-2?tab={str(selected_tab_index)}' if is_eels_tab else '/clustering-2'
         # clustering_2_class = LINK_ENABLE_ANIMATION_CLASS if is_eels_tab else LINK_DISABLE_CLASS
-        clustering_2_a_element = f'<a href="{clustering_2_href}" class="{LINK_ENABLE_ANIMATION_CLASS}">Clustering 2</a>'
+        clustering_2_a_element = f'<a href="{clustering_2_href}" class="{LINK_ENABLE_ANIMATION_CLASS}">Adv. Clustering</a>'
         
-        navigation_links.append((clustering_2_a_element,"Clustering 2"))
+        navigation_links.append((clustering_2_a_element,"Adv. Clustering"))
         
         quantification_href = f'/quantification?tab={str(selected_tab_index)}' if is_eels_tab else '/#'
         quantification_class = LINK_ENABLE_ANIMATION_CLASS if is_eels_tab else LINK_DISABLE_CLASS
